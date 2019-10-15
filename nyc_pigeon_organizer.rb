926eeffbfb73046bc03178f4d099b33a8d25335c
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 19:25:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  
+    org_hash = {}
+      data.each do |k, v|
+      attribute = k
+      v.each do |key, value|
+        specific = key
+        value.each do |name|
+          if org_hash[name] == nil
+            org_hash[name] = {}
+          end
+          if org_hash[name][attribute]
+            org_hash[name][attribute] << specific.to_s
+        else
+          org_hash[name][attribute] = [specific.to_s]
+        end
+        end
+      end
+    end
+    return org_hash
+  end
