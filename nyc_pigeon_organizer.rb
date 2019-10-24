fb02ec87dd06a25d243327343233b8f3dbc0b47d
Brad Nelson <bradnelson333@gmail.com>
2019-10-24 17:14:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |key, val|
+    #key1.each_key do |key2|
+      pigeon_list = data[key]
+    end
+  end
+
+  return pigeon_list
 end
