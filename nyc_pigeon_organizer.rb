5b5afc9e7a7b65b1e4ef309f073e95f52839d5c6
Joseph  Louie <joseph161216@gmail.com>
2019-10-26 00:11:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |k1, v1| #e.g. :color, {:purple => []}
+    v1.each do |attribute, names| #e.g. :purple, ["Theo"..."Lucky"]
+      names.each do |name|
+      #if pigeon_list doesn't have the name as a key
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        #if pigeon_list[name] doesn't have k1 as a key
+        if !pigeon_list[name][k1]
+          pigeon_list[name][k1] = []
+        end
+        if names.include?(name)
+          pigeon_list[name][k1].push(attribute.to_s)
+        end
+      end 
+    end
+  end
+  pigeon_list
 end
+
+
+
+
+
+
+
